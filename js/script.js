f7776298c6c7560f937dfce8538130bc69108f70
Micro10 <isadorabellaguarda@gmail.com>
2023-03-07 20:41:54 -0300

    parte dois

--- js/script.js
+++ js/script.js
@@ -1,41 +1,20 @@
-let ator = new Ator(1, "JOHN WAYNE");
-console.log(ator)
+let inputBuscarFilme = document.querySelector("#input-buscar-filme");
+let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
 
-let diretor = new Diretor(1, "Alfred Hitchcock");
-console.log(diretor)
+// btnBuscarFilme.onclick = () => {
+//     if(inputBuscarFilme.value.length > 0){
+//         console.log(inputBuscarFilme.value);
+//     }
+//     return false;
+// }
 
-let direcao = [
-    new Diretor(1, "Lana Wachowski"),
-    new Diretor(2, "Lilly Wachowski")
-]
-
-let elenco = [
-    new Ator(1, "Keanu Reeves"),
-    new Ator(2, "Carrie-Anne Moss"),
-    new Ator(3, "Laurence Fishburne"),
-    new Ator(4, "Joe Pantoliano"),
-    new Ator(5, "Hugo Weaving"),
-    new Ator(6, "Antony Ray Parker"),
-]
-
-let genero = ["ação", "aventura", "ficção cinetífica"]
-
-let sinopse = "Um jovem programador..."
-
-let cartaz = "link da imagem"
-
-let filme = new Filme(
-    1,
-    "Matrix",
-    1999,
-    genero,
-    102,
-    sinopse,
-    cartaz,
-    direcao,
-    elenco,
-    14,
-    null
-)
-
-console.log(filme)
+btnBuscarFilme.onclick = () => {
+    if(inputBuscarFilme.value.length > 0){
+        fetch("http://www.omdbapi.com/?apikey=d24fb342&s="+inputBuscarFilme.value)
+        .then((resp) => resp.json())
+        .then((resp) => {
+            console.log(resp);
+        })
+    }
+    return false;
+}
