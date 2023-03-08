21468345e40cdcbc1a41a0a9d8d3e1cbc14f9317
Micro10 <isadorabellaguarda@gmail.com>
2023-03-07 21:54:30 -0300

    metodo card filme atualizado

--- js/classes.js
+++ js/classes.js
@@ -29,7 +29,7 @@ class Filme{
     }
 
     getCard = () => {
-        let card = document.createElement("div").setAttribute("class", "card");
+        let card = document.createElement("div").setAttribute("class", "card mb-3 w-25");
         let imgCartaz = document.createElement("img").setAttribute("src", this.cartaz);
         imgCartaz.setAttribute("class", "card-img-top");
         let cardBody = document.createElement("div").setAttribute("class", "card-body");
@@ -38,13 +38,36 @@ class Filme{
 
         let pCardSinopse = document.createElement("p").setAttribute("class", "card-text");
         //todos os abaixo dentro de uma div
-        let cardGenero = document.createElement("div").setAttribute("");
-        let cardAno = document.createElement("div").setAttribute("");
-        let cardClassificacao = document.createElement("div").setAttribute("");
+        let cardDivInformacoes = document.createElement("div").setAttribute("style", "display: flex; justify-content:space-around;");
+
+        let cardGenero = document.createElement("div").setAttribute("style", "flex-grow: 1");
+        let cardAno = document.createElement("div").setAttribute("style", "flex-grow: 1");
+        let cardClassificacao = document.createElement("div").setAttribute("style", "flex-grow: 1");
+
         //fecha a div
         let cardLinkDetalhamento = document.createElement("a").setAttribute("href", "#");
 
 
     }
+
+
+    // <div class="card mb-3 w-25">
+    //           <img src="./imagens/tch1.jpg" class="card-img-top" alt="Cartaz do filme ${nome-do-filme}">
+    //           <div class="card-body">
+    //               <h6 class="card-title">Nome do filme (pegar da API)</h6>
+    //
+    //
+    //               <p class="card-text">Sinopse do filme (pegar da API)</p>
+    //
+    //               <div style="display: flex; justify-content:space-around;">
+    //                 
+    //                 <div style="flex-grow: 1;">Aventura (API)</div>
+    //                 <div style="flex-grow: 1;">2022 (API)</div>
+    //                 <div style="flex-grow: 1;">16A (API)</div>
+    //               
+    //              </div>
+    //               <a href="#">Detalhes (API)?</a>
+    //           </div>
+    //       </div>
     
 }
