3fec9fe22c427de5c8349bf853b8a611a6090a9e
Micro09 <isadorabellaguarda@gmail.com>
2023-03-14 19:49:38 -0300

    atualizações da aula anterior

--- js/script.js
+++ js/script.js
@@ -10,11 +10,40 @@ let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
 
 btnBuscarFilme.onclick = () => {
     if(inputBuscarFilme.value.length > 0){
-        fetch("http://www.omdbapi.com/?apikey=d24fb342&s="+inputBuscarFilme.value, {mode:"cors"})
+        let filmes = new Array();
+        fetch("http://www.omdbapi.com/?apikey=d24fb342&s="+inputBuscarFilme.value)
         .then((resp) => resp.json())
         .then((resp) => {
-            console.log(resp);
+            resp.Search.forEach((item) => {
+                console.log(item);
+                let filme = new Filme(
+                    item.imdbID,
+                    item.Title,
+                    item.Year,
+                    null,
+                    null,
+                    item.Poster,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+                );
+                filmes.push(filme);
+            });
+            listarFilmes(filmes);
         })
     }
     return false;
+}
+
+let listarFilmes = async (filmes) => {
+    let listaFilmes = await document.querySelector("#lista-filmes");
+    listaFilmes.innerHTML = "";
+    console.log(listaFilmes);
+    if(filmes.length > 0) {
+        filmes.forEach(async(filme) => {
+            listaFilmes.appendChild(await filme.getCard());
+        });
+    }
 }
